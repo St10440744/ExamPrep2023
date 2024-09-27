0788f53d319430bb2df499e620aefafb4bae7601
ST10440744 <ST10440744@vcconnect.edu.za>
2024-09-27 12:18:17 +0200

    first commit

---
+++ ExamPrep/2023/2023ExamPrep/src/main/java/com/mycompany/examprep/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examprep;
+
+/**
+ *
+ * @author jessj
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
